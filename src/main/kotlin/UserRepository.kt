720ced062df8a04dc52a8d26844a25ababc65150
yoshidanobutaka <nobutaka.yoshida@rakus-partners.co.jp>
2026-08-05 20:01:48 +0900

    feat: UserRepositoryを追加 (featureB)

--- src/main/kotlin/UserRepository.kt
+++ src/main/kotlin/UserRepository.kt
@@ -1,15 +1,6 @@
 package org.example
 
 class UserRepository {
-    // 簡易的なオンメモリDB
     private val users = mutableListOf<User>()
-
-    fun save(user: User) {
-        users.add(user)
-        println("User saved: ${user.name}")
-    }
-
-    fun findById(id: Int): User? {
-        return users.find { it.id == id }
-    }
+    fun save(user: User) { users.add(user) }
 }
