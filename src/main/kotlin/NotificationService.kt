5e8be8022178a689819a60f3b8b9391daebd6174
yoshidanobutaka <nobutaka.yoshida@rakus-partners.co.jp>
2026-08-07 09:39:22 +0900

    mock(notification): [仮実装] コンパイルを通すための通知サービスモック

---
+++ src/main/kotlin/NotificationService.kt
@@ -0,0 +1,8 @@
+package org.example
+
+// ★仮実装: 機能Bが完成するまでのダミー
+class NotificationService {
+    fun sendNotification(message: String) {
+        println("[MOCK] Notification sent: $message")
+    }
+}
